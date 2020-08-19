fab944079882da203604d9eaadeba5bf5bd74b4e
Eduardo Maciel <eduardomaciel007@hotmail.com>
2020-08-18 23:59:43 -0300

    Adicionando as variaveis do .env, e padronizando o module.exports

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
-const Sequelize = require('sequelize');
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+require('dotenv/config');
+module.exports = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: 'localhost',
     dialect:  'postgres',
     define: {
         timestamps : true
     }
-});
+}
 
 // new Sequelize
 // (`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`);
