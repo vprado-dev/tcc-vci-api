4ce7ced38cd5ad257b77fc8597fa8da541aded38
Eduardo Maciel <mrgamex49@gmail.com>
2020-10-26 09:34:10 -0300

    config database

--- config/database.js
+++ config/database.js
@@ -1,25 +1,13 @@
 require("dotenv/config");
 const Sequelize = require("sequelize");
 module.exports = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: "localhost",
-        dialect: "postgres"
-    },
-    define: {
-        timestamps: true
-    },
-    production: {
-        username: "rnlxptmxeveich",
-        password:
-            "ed8b2249591c52ff036962ac85aebb73765f2c837e631b5c5e4cfc68d34f66c5",
-        database: "d239l4doba6dom",
-        host: "ec2-3-233-236-188.compute-1.amazonaws.com",
-        port: 5432,
-        dialect: "postgres"
-    }
+    username: "rnlxptmxeveich",
+    password:
+        "ed8b2249591c52ff036962ac85aebb73765f2c837e631b5c5e4cfc68d34f66c5",
+    database: "d239l4doba6dom",
+    host: "ec2-3-233-236-188.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres"
 };
 
 // new Sequelize
