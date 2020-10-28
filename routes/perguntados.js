2e7ed199cd98cc63b88f81434f27d13cb4376961
Vinicius Prado <vinicavagna@gmail.com>
2020-10-28 15:37:20 -0300

    Criação de salas no socket server

--- routes/perguntados.js
+++ routes/perguntados.js
@@ -38,4 +38,34 @@ router.get("/:id", async function (req, res) {
 
 
 });
+
+router.get("/pergunta/:id", async function(req, res){
+    const questions = {
+        idquestion: req.params.id,
+    }
+
+    const result = await Question.findAll({
+        where: {
+            idquestion: questions.idquestion,
+        },
+    }).catch(function (err) {
+        res.json({
+            success: "false",
+            message: "Erro!",
+        });
+    });
+    if(result.length > 0){
+        // var busca = result.map((value, index)=>{
+        //     return value.dataValues;
+        // });
+
+        // var sort = Math.floor((Math.random()*(busca.length)));
+        return res.status(200).json({
+            success:"true",
+            message:"Pergunta selecionada",
+            data: result,
+        })
+    }
+});
+
 module.exports = router;
--- socket-server/index.js
+++ socket-server/index.js
@@ -10,16 +10,69 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
+var salas = [];
+
+var players = [];
+
 //Para madar pacotes entre cliente e servidor pode-se Usar:
 //- socket.on para 'Escutar' as requisicoes
 //- socket.emit para mandar requisicoes
+function createRoom(player){
+    console.log(player);
+    salas[salas.length] = {
+        num_sala : salas.length,
+        id_sala : Math.random()*1000,
+        players : [player]
+    };
+}
+
+function addPlayer(player){
+    salas[salas.length-1].players.push(player);
+}
+
 
 io.on("connection", (socket) => {
     console.log("Novo usuario conectado.");
+    players.push({
+        id : socket.client.id,
+        totens : [],
+        available : false
+    });
+    if(salas.length != 0){
+        if(salas[salas.length-1].players.length == 2 ){
+            createRoom(players[players.length-1]);
+        }else{
+           addPlayer(players[players.length-1]);
+        }
+    }else{
+        createRoom(players[players.length-1]);
+    }
+   
+    console.log(salas);
+    
+    
+    socket.on("errou", (args) =>{
+        if(players.length > 1){
+            for(var i = 0; i<players.length; i++){
+                if(players[i].id === args){
+                    players[i].available = false;
+                    if(!i){
+                        players[1].available = true;
+                    }else{
+                        players[0].available = true;
+                    }
+                }
+            }
+        }else{
+            console.log("Apenas um jogador conectado");
+        }
+        
+    });
 
     socket.on("disconnect", () => {
         console.log("Usuario desconectado.");
     });
 });
+
 
 server.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
