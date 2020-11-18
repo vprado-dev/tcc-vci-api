222a32759082069f657ac6460bb40ad1f3dd2f38
Vinicius Prado <vinicavagna@gmail.com>
2020-11-18 16:02:59 -0300

    Ranking query fix

--- routes/ranking.js
+++ routes/ranking.js
@@ -30,18 +30,21 @@ router.get("/all", async function (req, res) {
 })
 
 router.get("/:game", async function (req, res) {
+    const dados = req.params;
+    const idGame = dados.game;
+    console.log(dados);
     try{
         db.query(`SELECT * FROM ranking 
             INNER JOIN users ON ranking.iduser = users.iduser 
             INNER JOIN game ON ranking.idgame = game.idgame
-            WHERE game.idgame = ${req.params.game}
+            WHERE game.idgame = ${idGame}
             ORDER BY ranking.points DESC, ranking.time ASC;
         `)
         .then((resultados) => {
             res.json(resultados[0]);
         })
         .catch(function (err) {
-            console.log(err);
+            // console.log(err);
         });
     } catch (e) {
         res.json({
