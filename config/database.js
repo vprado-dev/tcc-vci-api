342d458eccdcf80996e0de7a96651d51eb66845e
Eduardo Maciel <mrgamex49@gmail.com>
2020-11-20 18:45:06 -0300

    Add two configs in database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
 require("dotenv/config");
 const Sequelize = require("sequelize");
-module.exports = {
+const producao = {
+    username: "rnlxptmxeveich",
+    password:
+        "ed8b2249591c52ff036962ac85aebb73765f2c837e631b5c5e4cfc68d34f66c5",
+    database: "d239l4doba6dom",
+    host: "ec2-3-233-236-188.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres"
+};
+const local = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -9,14 +18,8 @@ module.exports = {
     define: {
         timestamps: true
     }
-    // username: "rnlxptmxeveich",
-    // password:
-    //     "ed8b2249591c52ff036962ac85aebb73765f2c837e631b5c5e4cfc68d34f66c5",
-    // database: "d239l4doba6dom",
-    // host: "ec2-3-233-236-188.compute-1.amazonaws.com",
-    // port: 5432,
-    // dialect: "postgres",
 };
-
+// Aqui você só troca qnd quiser ir para produção ou local
+module.exports = local;
 // new Sequelize
 // (`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`);
