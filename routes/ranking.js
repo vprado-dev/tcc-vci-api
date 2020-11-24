3d0a7a161f9b6c55f52674c88a33662af9b195d8
Vinicius Prado <vinicavagna@gmail.com>
2020-11-24 19:30:02 -0300

    Bug fixes

--- routes/ranking.js
+++ routes/ranking.js
@@ -8,9 +8,31 @@ const { db }                = require('../config/objetos')
 const router                = express.Router();
 const jsonParser = bodyParser.json();
 
+router.get("/all", async function(req, res) {
+    try{
+        db.query(`
+            SELECT
+                ranking.idranking, ranking.iduser, ranking.time,  ranking.points,
+                game.name_game, users.name_user
+            FROM ranking INNER JOIN game ON ranking.idgame = game.idgame
+            INNER JOIN users ON ranking.iduser = users.iduser 
+            ORDER BY ranking.points DESC, ranking.time ASC;`)
+        .then((resultados) => {
+            res.json(resultados[0]);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    } catch (e) {
+        res.json({
+            success: false,
+            message: e.message,
+        });
+    }
+});
+
 router.get("/all/:id", async function (req, res) {
     const dados  = req.params.id;
-    console.log(dados);
     try{
         db.query(`SELECT
                 ranking.idranking, ranking.iduser, ranking.time,  ranking.points,
@@ -56,5 +78,31 @@ router.post("/rank-by-game", async function (req, res) {
         });
     }
 })
+
+router.get("/:game", async function (req, res) {
+    const dados = req.params;
+    const idGame = dados.game;
+    console.log(dados);
+    try{
+        db.query(`SELECT * FROM ranking 
+            INNER JOIN users ON ranking.iduser = users.iduser 
+            INNER JOIN game ON ranking.idgame = game.idgame
+            WHERE game.idgame = ${idGame}
+            ORDER BY ranking.points DESC, ranking.time ASC;
+        `)
+        .then((resultados) => {
+            res.json(resultados[0]);
+        })
+        .catch(function (err) {
+            // console.log(err);
+        });
+    } catch (e) {
+        res.json({
+            success: false,
+            message: e.message,
+        });
+    }
+})
+
 
 module.exports = router;
